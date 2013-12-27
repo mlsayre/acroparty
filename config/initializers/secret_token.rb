6c90a5d63a3f060287bb53d63be492b0a166130b
Matt Sayre <mlsayre@gmail.com>
2013-12-27 15:51:56 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acroparty::Application.config.secret_token = '23e253350a01eefad36dbd416e1939e8f4f4fe330f24ea16b8505f6a66249223b834c4a2ded9ec1884731ace03b60429e0c06abcee69cdd94f3cc05eae059b99'
