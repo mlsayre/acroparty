2a49d44594cbc4911b194eb4461e816c495a0f28
Matt Sayre <mlsayre@gmail.com>
2014-01-10 01:26:10 -0800

    sub info

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,5 +6,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.create!(params[:message])
+    PrivatePub.publish_to("/messages/familyroom", message: @message)
   end
 end
