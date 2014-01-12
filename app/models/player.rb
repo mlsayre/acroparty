d2dde559d20444835c70f79a8011c3b099f2ce59
Matt Sayre <mlsayre@gmail.com>
2014-01-11 16:05:08 -0800

    fix chrome backout issue

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ActiveRecord::Base
   attr_accessible :name, :room, :user_id
 
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => :room
 
   belongs_to :user
 end
