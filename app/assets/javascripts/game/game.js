6e126ec0ae02abfc9efa0e24f96a1f8cea2634c2
Matt Sayre <mlsayre@gmail.com>
2014-01-19 17:49:57 -0800

    beginning game

--- app/assets/javascripts/game/game.js
+++ app/assets/javascripts/game/game.js
@@ -1,12 +1,8 @@
-if (gon.playerlistcount > 1)
-  {
-  $("#acroletters").replaceWith('Waiting for at least 3 players...');
-  }
-else
-  {
-  $("#acroletters").hide();
-  };
-
-PrivatePub.subscribe("/familyroom/game", function(data, channel) {
-  $("#roundtext").replaceWith(data.game_message);
-});
+  if (gon.playerlistcount > 1)
+    {
+    $("#acroletters").replaceWith('Waiting for at least 3 players...');
+    }
+  else
+    {
+    $("#acroletters").show();
+    };
--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -8,12 +8,25 @@ class RoomController < ApplicationController
     @player = Player.create(:name => current_user.username, :room => "familyroom", :user_id => current_user.id)
     @playerlist = Player.where(:room => "familyroom")
     gon.playerlistcount = @playerlist.count
-    if @playerlist.count == 1
-      PrivatePub.publish_to "/familyroom/game", :game_message => "Only 1 player"
-    elsif @playerlist.count == 2
-      PrivatePub.publish_to "/familyroom/game", :game_message => "More than 1"
-    end
-
+    @letterpool = ["A","A","A","A","B","B","B","B","C","C","C","C","D","D","D","D",
+                  "E","E","E","E","E","F","F","F","F","G","G","G","G","H","H","H","H",
+                  "I","I","I","I","J","K","K","L","L","L","L","M","M","M","M","N",
+                  "N","N","N","O","O","O","P","P","P","P","Q","Q","R","R","R","S","S",
+                  "S","S","T","T","T","T","U","U","V","V","W","W","X","Y","Y","Z",
+                  "A","A","A","A","B","B","B","B","C","C","C","C","D","D","D","D",
+                  "E","E","E","E","E","F","F","F","F","G","G","G","G","H","H","H","H",
+                  "I","I","I","I","J","K","K","L","L","L","L","M","M","M","M","N",
+                  "N","N","N","O","O","O","P","P","P","P","Q","Q","R","R","R","S","S",
+                  "S","S","T","T","T","T","U","U","V","V","W","W","Y","Y","Z",
+                  "A","A","A","A","B","B","B","B","C","C","C","C","D","D","D","D",
+                  "E","E","E","E","E","F","F","F","F","G","G","G","G","H","H","H","H",
+                  "I","I","I","I","J","L","L","L","L","M","M","M","M","N",
+                  "N","N","N","O","O","O","P","P","P","P","Q","Q","R","R","R","S","S",
+                  "S","S","T","T","T","T","U","U","V","V","W","W","Y","Y","Z"]
+    @randomcategory = ["General", "Sports", "Food", "Movies", "Television", "History",
+                      "Music", "Art", "Animals", "Science", "Government", "Fashion",
+                      "Books", "Travel", "Celebrities", "Romance", "Technology",
+                      "Family", "School", "Nature", "Health"]
   end
 
   def destroyplayer
