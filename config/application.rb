108f6fd9ed464b9c174cfac50f56a87026ea6cce
Matt Sayre <mlsayre@gmail.com>
2014-03-07 01:02:00 -0800

    heroku 7

--- config/application.rb
+++ config/application.rb
@@ -65,7 +65,7 @@ module Acroparty
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     config.assets.precompile += %w( vendor/modernizr.js private_pub.js)
     end
