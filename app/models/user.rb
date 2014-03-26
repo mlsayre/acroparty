a8a0f032e5de18f4d4ad945058497e9e9cc5d4aa
Matt Sayre <mlsayre@gmail.com>
2014-03-26 03:09:36 -0700

    fb image

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ActiveRecord::Base
         user.avatar = auth["info"]["image"].sub("_normal", "")
       else
         user.email = auth["info"]["email"]
-        user.avatar = access_token.info.image
+        user.avatar = auth.info.image
       end
 
     end
