5fa9b334c42955473f9ce5406f7d6a1fce3f023b
Matt Sayre <mlsayre@gmail.com>
2014-04-06 14:53:14 -0700

    fixed tests

--- test/controllers/room_controller_test.rb
+++ test/controllers/room_controller_test.rb
@@ -1,14 +1,1 @@
-require "test_helper"
 
-class RoomControllerTest < ActionController::TestCase
-  test "should get FamilyRoom" do
-    get :FamilyRoom
-    assert_response :success
-  end
-
-  test "should get Sauna" do
-    get :Sauna
-    assert_response :success
-  end
-
-end
