ff45a21aefb0259273b6df2a74baee90438c857e
Matt Sayre <mlsayre@gmail.com>
2014-04-18 22:33:09 -0700

    timing cleanup

--- app/assets/javascripts/game/game.js
+++ app/assets/javascripts/game/game.js
@@ -57,7 +57,6 @@ function roundPrep(){
 
 // Writing and voting timer
 function wtimer(time) {
-  $("#rndstart").load("/room/familyroom #rndstart");
   var answercount=time; // Answer write time (s)
   var writecounter=setInterval(writingtimer, 1000);
   $("#timertext").css('color', 'black');
@@ -69,11 +68,9 @@ function wtimer(time) {
         gameplay50MUS.play();
       }
       else if (time == 61) {
-        roundWriteTimerStart();
         gameplayMUS.play();
       }
       else if (time == 81) {
-        roundWriteTimerStart();
         gameplay80MUS.play();
       }
     }
